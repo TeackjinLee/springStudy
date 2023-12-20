cad72690599af8211a42ad8b7eb21e292d66b69b
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2023-12-21 00:39:48 +0900

    part2 - 05.스프링mvc설

--- jex01/src/main/java/org/zerock/config/ServletConfig.java
+++ jex01/src/main/java/org/zerock/config/ServletConfig.java
@@ -1,6 +1,30 @@
 package org.zerock.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
-public class ServletConfig {
+@EnableWebMvc	// @EnableWebMvc 어노테이션을 사용하면 Spring Framework에서 여러 Config 값을 알아서 세팅해준다.
+@ComponentScan(basePackages = {"org.zerock.controller"})
+public class ServletConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		registry.viewResolver(bean);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/**");
+	}
 	
 }
