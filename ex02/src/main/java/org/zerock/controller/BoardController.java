9796c27b21f049d6a8692ccae3399660a22f8a6f
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2023-12-25 22:17:47 +0900

    part3-10-프리젠테이션(웹) 계층의CRUD 구현

---
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,66 @@
+package org.zerock.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Controller	// 스프링의 빈으로 인식할 수 있게 한다.
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor // 생성자를 만들고 자동으로 주입하도록 함.
+public class BoardController {
+//	@Setter(onMethod_ = {@Autowired})	// AllArgConstructor 이용 안할시 생성해줘야함
+	private BoardService boardService;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list", boardService.getList());
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("register : " + board);
+		boardService.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		log.info("/get");
+		model.addAttribute("board", boardService.get(bno));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify : " + board);
+		
+		if (boardService.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		} 
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove..." + bno);
+		if (boardService.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+}
