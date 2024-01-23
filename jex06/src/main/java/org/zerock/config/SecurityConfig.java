ad2156a0f3168d7a5f60b0318aa7d938bb3f40cb
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-01-23 22:34:35 +0900

    Part7-38-기존프로젝트에 스프링 시큐리티 접목하기

--- jex06/src/main/java/org/zerock/config/SecurityConfig.java
+++ jex06/src/main/java/org/zerock/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.zerock.security.CustomLoginSuccessHandler;
 import org.zerock.security.CustomUserDetailsService;
 
@@ -93,6 +95,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.logoutUrl("/customLogout")
 			.invalidateHttpSession(true)
 			.deleteCookies("remember-me", "JESSION_ID");
+		
+		http.rememberMe()
+			.key("zerock")
+			.tokenRepository(persistentTokenRepository())
+			.tokenValiditySeconds(604800);
 	};
+	
+	@Bean
+	public PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+		repo.setDataSource(dataSource);
+		return repo;
+	}
 	
 }
