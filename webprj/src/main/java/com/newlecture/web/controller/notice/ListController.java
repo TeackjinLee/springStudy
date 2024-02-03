2b18b4b145981642a019f7437b2c2e038204cd4a
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-03 11:47:08 +0900

    Spring MVC (스프링 웹 MVC) 강의 25 - 객체 DI를 Annotaion으로 변경하기

--- webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -14,11 +15,8 @@ import com.newlecture.web.service.jdbc.JDBCNoticeService;
 
 public class ListController implements Controller {
 	
-	private NoticeService noticeSerivce; 
-	
-	public void setNoticeService(NoticeService noticeSerivce) {
-		this.noticeSerivce = noticeSerivce;
-	}
+	@Autowired
+	private NoticeService noticeSerivce;
 
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
